a7c4f309af648e4a119c59833972494a5c58c084
JuanSoftware <js33neospro@gmail.com>
2023-05-17 15:56:48 -0400

    nao sei

--- Q1.c
+++ Q1.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-main(){
+int main(){
 	int n[3], resultado, i;
+	entrada(n, resultado);
+	
+}
+int entrada(int n[3], int resultado, int i){
 	for(i=3;i>0;i--){
 		printf("Digite um numero:\n");
 		scanf("%d", &n[i]);
 		resultado= resultado + n[i];
-		
 	}
 	printf("%d", resultado);
 }
+
